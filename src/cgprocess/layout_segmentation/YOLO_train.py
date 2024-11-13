8443af83c0965ee05687d9ba8112d182f7ecfa72
NKerkfeld <s6nikerk@uni-bonn.de>
2024-11-13 19:32:17 +0100

    update train

--- src/cgprocess/layout_segmentation/YOLO_train.py
+++ src/cgprocess/layout_segmentation/YOLO_train.py
@@ -6,7 +6,10 @@ def main():
     model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
     # Train the model
-    results = model.train(data="data/YOLO_dataset/CGD.yaml", epochs=10, imgsz=2048)
+    results = model.train(data="data/YOLO_dataset/CGD.yaml",
+                          epochs=10,
+                          imgsz=2048,
+                          batch=8)
 
     print(results)
     
