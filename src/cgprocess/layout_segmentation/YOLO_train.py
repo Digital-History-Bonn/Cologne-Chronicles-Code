ca52ce006b59142af81a2c203ee2da8d812688cc
NKerkfeld <s6nikerk@uni-bonn.de>
2024-11-16 15:11:49 +0100

    train segment

--- src/cgprocess/layout_segmentation/YOLO_train.py
+++ src/cgprocess/layout_segmentation/YOLO_train.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 
 
-def main():
+def main(task: str):
     # Load a model
-    model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolov8n.pt" if task == 'detect' else "yolov8n-seg.pt")  # load a pretrained model (recommended for training)
 
     # Train the model
-    results = model.train(data="data/YOLO_dataset/CGD.yaml",
-                          epochs=100,
-                          imgsz=2048,
+    results = model.train(data="data/YOLO_dataset/CGD.yaml" if task == 'detect' else "data/YOLO_Textlines/CGD.yaml",
+                          epochs=500,
+                          imgsz=2048 if task == 'detect' else 1024,
                           batch=16,
                           device=[0, 1])
 
    
 if __name__ == '__main__':
-    main()
+    main(task='segment')
