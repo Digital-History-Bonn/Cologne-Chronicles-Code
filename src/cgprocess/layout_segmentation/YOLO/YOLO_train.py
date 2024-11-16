57d0b48d93195cd0bdd8fc4a802f198ed553f4dc
NKerkfeld <s6nikerk@uni-bonn.de>
2024-11-16 23:55:44 +0100

    seed prediction

--- src/cgprocess/layout_segmentation/YOLO/YOLO_train.py
+++ src/cgprocess/layout_segmentation/YOLO/YOLO_train.py
@@ -4,29 +4,39 @@ import torch
 from ultralytics import YOLO
 
 
-def main(task: str):
-    # Load a model
-    model_file = {"detect": "yolov8n.pt",
-                  "segment": "yolov8n-seg.pt",
-                  "pose": "yolov8n-pose.pt"}[task]
+def main():
+    args = get_args()
+
+    # get weights or params
+    if args.seed is None:
+        model_file = {"detect": "yolov8n.pt",
+                      "segment": "yolov8n-seg.pt",
+                      "pose": "yolov8n-pose.pt"}[args.task]
+    else:
+        model_file = {"detect": "yolov8n.yaml",
+                      "segment": "yolov8n-seg.yaml",
+                      "pose": "yolov8n-pose.yaml"}[args.task]
+
+    # Init a model
     model = YOLO(model_file)
 
     # Train the model
     yaml = {"detect": "data/YOLO_Layout/CGD.yaml",
             "segment": "data/YOLO_Textlines/CGD.yaml",
-            "pose": "data/YOLO_Baselines/CGD.yaml"}[task]
+            "pose": "data/YOLO_Baselines/CGD.yaml"}[args.task]
 
     imgz = {"detect": 2048,
             "segment": 1024,
-            "pose": 1024}[task]
+            "pose": 1024}[args.task]
 
     devices = list(range(torch.cuda.device_count())) if torch.cuda.is_available() else 'cpu'
 
     model.train(data=yaml,
-                epochs=500 if task == 'detect' else 200,
+                epochs=500 if args.task == 'detect' else 200,
                 imgsz=imgz,
                 batch=8 * len(devices) if torch.cuda.is_available() else 8,
-                device=devices)
+                device=devices,
+                seed=args.seed)
 
 
 def get_args() -> argparse.Namespace:
@@ -46,9 +56,16 @@ def get_args() -> argparse.Namespace:
         help="task to train must be \"detect\" or \"segment\" or \"pose\""
     )
 
+    parser.add_argument(
+        "--seed",
+        "-s",
+        type=int,
+        default=None,
+        help="Seed for training. If seed is not set pretrained weights are used."
+    )
+
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    args = get_args()
-    main(task=args.task)
+    main()
