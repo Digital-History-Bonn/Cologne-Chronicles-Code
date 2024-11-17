dc940d5830b4e766efb73ed6e06d10c23a6cea85
NKerkfeld <s6nikerk@uni-bonn.de>
2024-11-17 14:08:48 +0100

    activate seeding

--- src/cgprocess/layout_segmentation/YOLO/train.py
+++ src/cgprocess/layout_segmentation/YOLO/train.py
@@ -8,7 +8,7 @@ def main():
     args = get_args()
 
     # get weights or params
-    if args.seed is None:
+    if args.seed == 0:
         model_file = {"detect": "yolov8n.pt",
                       "segment": "yolov8n-seg.pt",
                       "pose": "yolov8n-pose.pt"}[args.task]
@@ -25,7 +25,7 @@ def main():
             "segment": "data/YOLO_Textlines/CGD.yaml",
             "pose": "data/YOLO_Baselines/CGD.yaml"}[args.task]
 
-    imgz = {"detect": 640,
+    imgz = {"detect": 2048,
             "segment": 1024,
             "pose": 1024}[args.task]
 
@@ -36,7 +36,7 @@ def main():
                 imgsz=imgz,
                 batch=8 * len(devices) if torch.cuda.is_available() else 8,
                 device=devices,
-                # seed=args.seed,
+                seed=args.seed,
                 cache=True)
 
 
@@ -61,7 +61,7 @@ def get_args() -> argparse.Namespace:
         "--seed",
         "-s",
         type=int,
-        default=None,
+        default=0,
         help="Seed for training. If seed is not set pretrained weights are used."
     )
 
